b1bd90cc297c867121698a1ef6379a45c9d8ccbe
silver-sparrow-studios <silversparrowstudios.pvt.ltd@hotmail.com>
2025-05-05 16:43:06 +0500

    set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    reactStrictMode: true, // Example Next.js setting
+    // Add other Next.js settings here
+  };
+  
